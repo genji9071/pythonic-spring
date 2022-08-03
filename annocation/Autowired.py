a00b6f37456cdc1fb0bbee88140448edaf41fabd
tianhaozhang <zhangtianhao@tezign.com>
2022-08-03 16:11:13 +0800

    0.0.0.1版： - add warning if bean initialize failed.

--- annocation/Autowired.py
+++ annocation/Autowired.py
@@ -14,6 +14,8 @@ def Autowired(func=None, bean_modules: List[ModuleType] = [], bean_ids: List[str
         bean_set = set(bean_ids + list(map(lambda x: x.__name__.replace(".", "_"), bean_modules)))
         for bean_id in bean_set:
             bean = BeanFactory.get_bean_by_name(bean_id)
+            if not bean:
+                print(f"Invalid bean initialize! bean_id: {bean_id}")
             slots[bean_id] = bean
         args[0].__dict__.update(slots)
         print(f'Autowired func: {func}')
