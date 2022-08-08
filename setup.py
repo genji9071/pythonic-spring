49359d6fb8647d6bcecb597ae276ba50993a0c6e
tianhaozhang <zhangtianhao@tezign.com>
2022-08-08 15:20:07 +0800

    0.0.0.2版： - 重构代码结构，使之更容易使用 - 打包

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pythonicspring",
-    version="0.0.0.1",
+    name="pythonic-spring",
+    version="0.0.0.2",
     description="A python-made framework like Spring in Java.",
     author="Tianhao Zhang",
     author_email="genji9071@gmail.com",
@@ -12,7 +12,7 @@ setup(
         "Source": "https://github.com/genji9071/pythonic-spring/",
         "Tracker": "https://github.com/genji9071/pythonic-spring/issues"
     },
-    packages=find_packages(exclude=["test"]),
+    packages=["pythonicspring"],
     install_requires=["pydantic==1.8.2"],
     python_requires=">=3"
 )
