fb206949b9e8da32016249769da608f2fe120181
tianhaozhang <zhangtianhao@tezign.com>
2022-08-09 10:28:44 +0800

    0.0.1.2版： - 为了发上pypi，版本干到了0.0.1.2

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="pythonic-spring",
     version="0.0.1.2",
-    description="A python-made framework like Spring in Java.",
+    description="A python-made framework which is like Spring in Java.",
     author="Tianhao Zhang",
     author_email="genji9071@gmail.com",
     license="MIT",
@@ -14,5 +17,7 @@ setup(
     },
     packages=find_packages(),
     install_requires=["pydantic==1.8.2"],
-    python_requires=">=3"
+    python_requires=">=3",
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
